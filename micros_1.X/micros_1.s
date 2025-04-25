0e495d18c010300c18c3b7545c3883fb61444675
Andres Morales <amoralesma@unal.edu.co>
2025-04-25 00:52:08 -0500

    [Update]

--- micros_1.X/micros_1.s
+++ micros_1.X/micros_1.s
@@ -27,26 +27,34 @@
 
     aux1    EQU     0x01          ; Variable temporal 1
     aux2    EQU     0x02          ; Variable temporal 2
-    aux3    EQU     0x03          ; Variable temporal 2
-    aux4    EQU     0x04          ; Variable temporal 2
+    aux3    EQU     0x03          ; Variable temporal 3
+    aux4    EQU     0x04          ; Variable temporal 4
+    
 
 ;===============================================================
 ; 2. Código principal 
 ;===============================================================     
-
+    
+    ORG     0x0000
+    GOTO    rst
+    
     PSECT code, class=CODE, reloc=2
 
  ; Para fines de organización, cada etiqueta representará un 
  ; ejercicio. Se nombrará ex_<i> 
     
- 
+    rst: 
+	clrf	aux1
+	clrf	aux2
+	clrf	aux3
+	clrf	aux4
+	
     ex_1:			
         movlw	    0x07
 	movwf	    aux1
 	movlw	    0x02
 	addwf	    aux1
-	
-    
+ 
     ex_2: 
 	movlw	    0x08
 	movwf	    aux1
@@ -57,11 +65,18 @@
 	movf	    aux1, w	; Movemos a WREG para operar 
 	addwf	    aux2, w
 	
+    ex_3: 
+	movlw	    0x05
+	movwf	    aux1
+	movf	    aux1, w	; cargo a WREG
+	sublw	    0x09
+	
+	movwf	    0x00
+	;sublw	    0x07
+	
+    ex_4: 
 	
 	
-	
-    
-
     loop: 
         goto    $              ; Bucle infinito
 	
