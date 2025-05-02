3f38abdbdb2647cdaf83a6cd7941dcb806806ac5
Oscar Guzmán <oguzmanv@unal.edu.co>
2025-05-02 13:11:15 -0500

    Adición ejercicios pares

--- Prueba_2.X/nbproject/Package-default.bash
+++ Prueba_2.X/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/micros_2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=micros_2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=micros2.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Prueba_2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=Prueba_2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=prueba2.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/micros2.x/bin
+makeDirectory ${TMPDIR}/prueba2.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/micros2.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/prueba2.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/micros2.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/prueba2.x.tar *
 checkReturnCode
 
 # Cleanup
--- micros_1.X/micros_1.s
+++ micros_1.X/micros_1.s
@@ -3,7 +3,7 @@
 ; Authors     : Andrés Morales, Oscar Guzmán 
 ; Date        : 2025-04-23
 ; Lab         : 1 
-; Version     : 1.0
+; Version     : 2.0
 ; Description : Introducción a las instrucciones en ensamblador. 
 ;               Compilado con pic-as (diferente a mpasm clásico
 ;               de la versión 3.2 de MPLAB X).
@@ -12,11 +12,9 @@
 ; Version  Date        Author(s)              Description
 ; -------  ----------  ---------------------  ----------------------------
 ; 1.0      2025-04-24  A. Morales, O. Guzmán  Versión inicial.
+; -------  ----------  ---------------------  ----------------------------
+; 2.0      2025-05-02  A. Morales, O. Guzmán  Adición ejercicios pares.
 ;===============================================================
-    
-;Versión 2
-;Verision 3 
-;:b
     
 ;===============================================================
 ; 0. Configuración de archivo     
@@ -54,14 +52,14 @@ PSECT code, class=CODE, reloc=2
 	addwf	    aux1
  
     ex_2: 
-	movlw	    0x08
-	movwf	    aux1 ; Condiocion que dice robin
+	movlw 0x08; 8->W
+        movwf aux1; W->aux1
+    
+	movlw 0x0A; 10->W
+	movwf aux2; W->aux2
 	
-	movlw	    0x0A
-	movwf	    aux2
-	
-	movf	    aux1, w	; Movemos a WREG para operar 
-	addwf	    aux2, w
+	movf aux1, w; aux1 ->w
+        addwf aux2, w; W + aux2 -> W
 	
     ex_3: 
 	movlw	    0x05
@@ -73,7 +71,83 @@ PSECT code, class=CODE, reloc=2
 	;sublw	    0x07
 	
     ex_4: 
+	movlw 0x06; 6->W
+	movwf aux1; W->aux1
+         
+	movlw 0x04; 4->W
+	movwf aux2; W->aux2
+    
+	movf aux1,w; aux1->W
+	subwf aux2; aux2-W -> aux2
+
+    ex_6:
+	movlw 0x0C; 12->W
+	movwf aux1; W->aux1
+    
+	movlw 0x0F; 15->W
+	movwf aux2; W->aux2
+    
+	movf aux1,w; aux1 -> W    
+	mulwf aux2; W * aux2 -> PRODL    
+	movf PRODL, w; PRODL -> W
+	movwf aux2; 
 	
+    ex_8:
+	movlw 0x0C; 12->W
+	movwf aux1; W->aux1
+    
+	negf aux1;  -aux1 + 1 -> aux1$
+	
+    ex_10:
+	movlw 0x14; 20->W      0001 0100
+	movwf aux1; W->aux1    0011 1000
+    
+	movlw 0x38; 56->W      0011 1100 = 3C = 48 + 12 = 60
+	movwf aux2; W->aux2
+    
+	movf aux1,w; aux1->W
+	iorwf aux2, w; W or aux2 -> W
+	
+    ex_12:
+	movlw 0x64; 100->W     0110 0100
+	movwf aux1; W->aux1    0010 1110
+			 ;     0010 0100 = 24 = 36
+	movlw 0x2D; 45->W
+	movwf aux2; W->aux2
+    
+	movf aux1, w; aux1->W
+	andwf aux2,w; W and aux2 -> W
+	
+    ex_14:
+	movlw 0x11; 17->W     0001 0001
+	movwf aux1; W->aux1   0101 1010
+    
+	movlw 0x5A; 90->W     0100 1011 = 4B = 75
+	movwf aux2; W->aux2
+    
+	movf aux1, w; aux1->W
+	xorwf aux2, w; w xor aux2 -> W
+	
+    ex_16: 
+	movlw 0x12; 18->W
+	movwf aux1; W->aux1
+    
+	movlw 0x3C; 60->W
+	movwf aux2; W->aux2
+    
+	movlw 0x10; 16->W
+	movwf aux3; W->aux3
+    
+	movf aux1,w; aux1 -> W
+	addwf aux2; W(aux1) + aux2 -> aux2
+    
+	movf aux3,w; aux3 -> W
+	sublw 0b11010; 0b11010 - W -> W    ; 0b11010 = 26
+	mullw 0x03; 0x03 * W -> PRODL
+    
+	movf PRODL,W; PRODL->w
+	addwf aux2, w; aux2 + W -> W
+	movwf aux4; W -> aux4
 	
     loop: 
         goto    ex_1              ; Bucle infinito
