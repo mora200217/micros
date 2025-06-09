cba7b5c59c4b7bbc7d1f9c815a4ac190bdbfe5fd
Oscar Guzmán <oguzmanv@unal.edu.co>
2025-06-08 21:43:14 -0500

    [Update] Contador con TMR0 y conmutación con punteros (direccionamiento indirecto)

--- Lab3.X/nbproject/Package-default.bash
+++ Lab3.X/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Lab2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=Lab2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=lab2.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Lab3.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=Lab3.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=lab3.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/lab2.x/bin
+makeDirectory ${TMPDIR}/lab3.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/lab2.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/lab3.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/lab2.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/lab3.x.tar *
 checkReturnCode
 
 # Cleanup
